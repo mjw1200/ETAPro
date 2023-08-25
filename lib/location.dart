7374a41eff634e6c29f153e896586e82834576cd
Mike Waldron <mjw1200@yahoo.com>
2023-08-24 20:41:24 -0600

    Got the list of updates working!

--- lib/location.dart
+++ lib/location.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 
@@ -5,39 +8,32 @@ class Location extends StatefulWidget {
   const Location({super.key});
 
   @override
-  State<StatefulWidget> createState() => _LocationState();
+  State<StatefulWidget> createState() => LocationState();
 }
 
-class _LocationState extends State<Location> {
+class LocationState extends State<Location> {
   final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
-  Position _lastPos = Position(
-      longitude: 0.0,
-      latitude: 0.0,
-      timestamp: DateTime.now(),
-      accuracy: 0.0,
-      altitude: 0.0,
-      heading: 0.0,
-      speed: 0.0,
-      speedAccuracy: 0.0);
+  List<Text> _updates = [];
+  var quackCount = 1;
 
-  @override
-  void initState() {
-    // _lastPos = await geolocatorPlatform.getCurrentPosition();
-    super.initState();
+  LocationState() : super() {
+    Timer.periodic(const Duration(milliseconds: 5000), setCurrentPosition);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      OutlinedButton(onPressed: setCurrentPosition, child: const Text('Push to Locate')),
-      const Padding(padding: EdgeInsets.all(8.0)),
-      Text('Lat: ${_lastPos.latitude} Lon: ${_lastPos.longitude}')
-    ]);
+    List<Widget> chilluns = [];
+    chilluns.add(Text('Update count: ${_updates.length}'));
+    chilluns.addAll(_updates);
+
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: chilluns);
   }
 
-  Future<void> setCurrentPosition() async {
+  void setCurrentPosition(Timer t) async {
     setState(() {});
 
-    _lastPos = await _geolocatorPlatform.getCurrentPosition();
+    log('Quack ${this.quackCount++}');
+    var pos = await _geolocatorPlatform.getCurrentPosition();
+    _updates.add(Text('Lat: ${pos.latitude} Lon: ${pos.longitude} Speed: ${pos.speed} Time: ${pos.timestamp}'));
   }
 }
