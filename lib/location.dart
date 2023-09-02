747dc5585f02c5446ab3d6399670bc1cde1587cb
Mike Waldron <mjw1200@yahoo.com>
2023-09-01 19:24:09 -0600

    Finally got a non-default font to work

--- lib/location.dart
+++ lib/location.dart
@@ -13,7 +13,15 @@ class Location extends StatefulWidget {
 
 class LocationState extends State<Location> {
   final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
-  final List<Text> _updates = [];
+  Position _pos = Position(
+      longitude: 0,
+      latitude: 0,
+      timestamp: DateTime.now(),
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
 
   LocationState() : super() {
     Timer.periodic(const Duration(milliseconds: 5000), setCurrentPosition);
@@ -21,17 +29,31 @@ class LocationState extends State<Location> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> chilluns = [];
-    chilluns.add(Text('Update count: ${_updates.length}'));
-    chilluns.addAll(_updates);
+    // List<Widget> chilluns = [];
+    // chilluns.add(Text('Update count: ${_updates.length}'));
+    // chilluns.addAll(_updates);
 
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: chilluns);
+    // return Column(mainAxisAlignment: MainAxisAlignment.center, children: chilluns);
+    // List<Widget> child = [];
+    // child.add();
+    return Text(_pos.speed.toString(), style: const TextStyle(fontSize: 72, fontFamily: 'Adlam'));
+    // return Row(
+    //     mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: child);
+    // return Column(
+    //     mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: child);
   }
 
   void setCurrentPosition(Timer t) async {
     var pos = await _geolocatorPlatform.getCurrentPosition();
-    _updates.add(Text('Lat: ${pos.latitude} Lon: ${pos.longitude} Speed: ${pos.speed} Time: ${pos.timestamp}'));
-    log('Update count: ${_updates.length}');
+    _pos = Position(
+        longitude: 0,
+        latitude: 0,
+        timestamp: DateTime.now(),
+        accuracy: 0,
+        altitude: 0,
+        heading: 0,
+        speed: pos.speed * 2.237, // convert speed from m/s to mph
+        speedAccuracy: 0);
 
     setState(() {});
   }
