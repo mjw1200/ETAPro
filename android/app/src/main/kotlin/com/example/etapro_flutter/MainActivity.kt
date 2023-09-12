69f5048f0cadf2ba2d5e19b1786ef4c4f2ad2d8b
Mike Waldron <mjw1200@yahoo.com>
2023-09-12 11:31:13 -0600

    It's working pretty well

--- android/app/src/main/kotlin/com/example/etapro_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/etapro_flutter/MainActivity.kt
@@ -19,21 +19,26 @@ class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        
+        // TODO: Getting this client too many times? Do once and save the result?
         val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             call, result ->
 
             if (call.method == "getCurrentLocation") {
-                fusedLocationClient
-                .lastLocation
+                // 102 is PRIORITY_BALANCED_POWER_ACCURACY
+                // https://developers.google.com/android/reference/com/google/android/gms/location/Priority
+                fusedLocationClient.getCurrentLocation(102, null)
                 .addOnSuccessListener { 
                     location: Location? ->
                     result.success(doubleArrayOf(location!!.latitude, location.longitude))
                 }
-                
-                // val lat: Double = 1.23
-                // val lon: Double = 4.56
-                // result.success(doubleArrayOf(lat, lon))
+                // fusedLocationClient
+                // .lastLocation
+                // .addOnSuccessListener { 
+                //     location: Location? ->
+                //     result.success(doubleArrayOf(location!!.latitude, location.longitude))
+                // }
             }
             else {
                 result.notImplemented()
--- lib/location.dart
+++ lib/location.dart
@@ -22,6 +22,7 @@ class LocationState extends State<Location> {
   final int _updateInterval = 5000; // ms
   // double _speed = 0.001; // ensure non-zero
   Float64List _location = Float64List.fromList([88.88, 99.99]);
+  List<Widget> _chilluns = [];
 
   LocationState() : super() {
     Timer.periodic(Duration(milliseconds: _updateInterval), periodicUpdate);
@@ -29,34 +30,37 @@ class LocationState extends State<Location> {
 
   @override
   Widget build(BuildContext context) {
-    const TextStyle style = TextStyle(fontSize: 18, fontFamily: 'Adlam');
     // var mphSpeed = (_speed * 2.23694).round();
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
+    return Column(mainAxisAlignment: MainAxisAlignment.start, children: _chilluns // [
         // Text(
         //   'Speed (mph): ${mphSpeed.toStringAsFixed(0)}',
         //   style: style,
         // ),
-        Text(
-          'Lat: ${_location[0]} / Lon: ${_location[1]}',
-          style: style,
-        ),
-      ],
-    );
+        //   Text(
+        //     'Lat: ${_location[0]} / Lon: ${_location[1]}',
+        //     style: style,
+        //   ),
+        // ],
+        );
   }
 
   void periodicUpdate(Timer t) async {
     // Music Villa: 45.67973305878354, -111.02897198805852
     // The Office: 45.660500254366056, -110.55933360340568
     // Touchmark: 46.57686096725291, -111.98913545917074
+    const TextStyle style = TextStyle(fontSize: 18, fontFamily: 'Adlam');
 
     try {
       Float64List location = await platform.invokeMethod('getCurrentLocation');
 
       setState(() {
         _location = location;
+
+        _chilluns.add(Text(
+          'Lat: ${_location[0]} / Lon: ${_location[1]}',
+          style: style,
+        ));
       });
     } on PlatformException catch (e) {
       stderr.write('Caught a PlatformException in periodicUpdate: ${e.message}');
