52d5440e391dffed3542ec7382678b29c7122c8e
Mike Waldron <mjw1200@yahoo.com>
2023-09-12 10:38:40 -0600

    Checkpoint

--- android/app/src/main/kotlin/com/example/etapro_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/etapro_flutter/MainActivity.kt
@@ -16,40 +16,28 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "native_gps"
-    // private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-                call, result ->
+            call, result ->
 
-                val lat: Double = 1.23
-                val lon: Double = 4.56
-                result.success(doubleArrayOf(lat, lon))
-                // result.success(arrayOf(1.23, 4.56))
-
-//             if (call.method == "getBatteryLevel") {
-//                 val batteryLevel = getBatteryLevel()
-
-//                 if (batteryLevel != -1) {
-//                     result.success(batteryLevel)
-//                 } else {
-//                     result.error("UNAVAILABLE", "Battery level not available.", null)
-//                 }
-//             } else if (call.method == "getCurrentLocation") {
-//                 try {
-//                     fusedLocationClient.lastLocation
-//                         .addOnSuccessListener { location: Location? ->
-//                             result.success(arrayOf(2.1, 7.9))
-// //                            result.success(arrayOf(location!!.latitude, location.longitude))
-//                         }
-//                 }
-//                 catch (e: SecurityException) {
-//                     result.error("PERMISSION", "Location permissions unavailable", null)
-//               }
-//             } else {
-//                 result.notImplemented()
-//             }
+            if (call.method == "getCurrentLocation") {
+                fusedLocationClient
+                .lastLocation
+                .addOnSuccessListener { 
+                    location: Location? ->
+                    result.success(doubleArrayOf(location!!.latitude, location.longitude))
+                }
+                
+                // val lat: Double = 1.23
+                // val lon: Double = 4.56
+                // result.success(doubleArrayOf(lat, lon))
+            }
+            else {
+                result.notImplemented()
+            }
         }
     }
 
