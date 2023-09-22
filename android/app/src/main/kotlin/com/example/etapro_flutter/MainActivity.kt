7da1ac88abf5147fe850a6073687c90b06fc5cea
Mike Waldron <mjw1200@yahoo.com>
2023-09-22 12:23:03 -0600

    Use a real constant for PRIORITY_HIGH_ACCURACY

--- android/app/src/main/kotlin/com/example/etapro_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/etapro_flutter/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.example.etapro_flutter
 
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
 import android.location.Location
-import android.os.BatteryManager
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -26,14 +20,13 @@ class MainActivity: FlutterActivity() {
             call, result ->
 
             if (call.method == "getCurrentLocation") {
-                // 102 is PRIORITY_BALANCED_POWER_ACCURACY
-                // 100 is PRIORITY_HIGH_ACCURACY
+                // We should already have permissions
                 // https://developers.google.com/android/reference/com/google/android/gms/location/Priority
-                fusedLocationClient.getCurrentLocation(100, null)
-                .addOnSuccessListener { 
-                    location: Location? ->
+                fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+                .addOnSuccessListener {
+                        location: Location? ->
                     result.success(doubleArrayOf(location!!.latitude, location.longitude))
-                }                
+                }
             }
             else {
                 result.notImplemented()
